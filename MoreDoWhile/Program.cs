08bb3ee6c4d1631e52936fb48ec70fe359b6f7ca
Todd Patton <64681278+Todd-ops@users.noreply.github.com>
2020-06-23 20:12:41 -0500

    Add project files.

---
+++ MoreDoWhile/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MoreDoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			var random = new Random();
+			var randomNumber = random.Next(1, 50);
+			int userInput;
+
+			do
+			{
+				Console.WriteLine($"Please guess a number between 1 - 50:");
+
+				userInput = int.Parse(Console.ReadLine());
+
+				if (userInput > randomNumber)
+				{
+					Console.WriteLine("Too High");
+				}
+				else if (userInput < randomNumber)
+				{
+					Console.WriteLine("Too Low");
+				}
+				else
+				{
+					Console.WriteLine("Nice Work!");
+				}
+			} while (userInput != randomNumber);
+		}
+    }
+}
